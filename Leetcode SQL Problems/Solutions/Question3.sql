f4d107c48089ac388f99fefeb3ec7572dddc2c47
akshya095 <akshyakc095@gmail.com>
2024-12-20 17:18:42 +0545

    Updated

--- Leetcode SQL Problems/Solutions/Question3.sql
+++
@@ -1,7 +0,0 @@
--- SQL Script for Question 3
--- Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
-;
-SELECT email
-FROM Person
-GROUP BY email
-HAVING COUNT(*) = 2;
